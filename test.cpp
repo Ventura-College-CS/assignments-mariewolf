83f0b5b13cbc59d65c26fce9693a46e9ced6894d
mariewolf <77356063+mariewolf@users.noreply.github.com>
2021-08-16 14:40:16 -0700

    test

--- test.cpp
+++ test.cpp
@@ -1,6 +1,27 @@
-#include <iostream>;
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main(){
-    cout << "hello world" << endl;
+int main()
+{
+    int num, i, range1, range2;
+    
+    cout <<"Beginning of range: ";
+    cin >> range1;
+    cout <<"End of range: ";
+    cin >> range2;
+    
+    for(num=range1; num<=range2; num++) {
+        for(i=2; i<(num/2); i++) {
+            if (num % i == 0)
+                break;
+        }
+        
+        if(i == num/2)
+            cout << num << " - This number is prime.\n";
+        else
+            cout << num << " - Not prime.\n";
+    }
+
+    return 0;
 }
