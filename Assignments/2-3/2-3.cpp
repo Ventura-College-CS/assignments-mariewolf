35a7bca5849dad4a6861aa9408283634f6b975d7
mariewolf <77356063+mariewolf@users.noreply.github.com>
2021-10-11 23:24:27 -0700

    edited 2-3 & 2-4

--- Assignments/2-3/2-3.cpp
+++ Assignments/2-3/2-3.cpp
@@ -6,25 +6,29 @@ using namespace std;
 double *makeNumbers(int N);
 void printNumbers(double *, int N);
 void sortNumbers(double *, int N);
+void deleteNumbers(double *);
 
 int main() {
     const int N = 10;
     double *ptr;
 
     ptr = makeNumbers(N);
+    cout << "Array: ";
     printNumbers(ptr, N);
     sortNumbers(ptr, N);
+    cout << "\nSorted: ";
     printNumbers(ptr, N);
+
+    deleteNumbers(ptr);
 
 }
 
 double *makeNumbers(int N) {
-    double *ptr;
-    ptr = new double[N];
+    double *ptr = new double[N];
 
     srand(time(0));
     for(int i=0; i<N; i++) {
-        *(ptr+i) = rand() % 100; //generate random numbers
+        *(ptr+i) = (rand() % 1000)/10.0; //generate random double values
     }
 
     return ptr;
@@ -32,7 +36,7 @@ double *makeNumbers(int N) {
 
 void printNumbers(double *ptr, int N) {
     for (int i=0; i<N; i++)
-        cout << *(ptr+i) << "\t";
+        cout << *(ptr+i) << " ";
     cout << endl;
 }
 
@@ -43,4 +47,8 @@ void sortNumbers(double *ptr, int N) {
         *(ptr+i) = *(ptr+N-i-1);
         *(ptr+N-i-1) = tempHolder;
     }
+}
+
+void deleteNumbers(double *ptr) {
+    delete[] ptr;
 }
--- Assignments/2-4/2-4.cpp
+++ Assignments/2-4/2-4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 using namespace std;
 
 const int 	MAX_LEN = 20;
@@ -10,46 +11,70 @@ struct Students {
 	double 	scores[NUM_SCORES];
 };
 
-Students *makeStudents(int );
+Students *makeStudents(int);
 void printStudents(Students * const, int);
 void sortStudents(Students * const, int);
 
 
-int main()
-{
-    //adjust students.txt file
-    
+int main() {
 	const int N = 10;
 	Students *ptr;
 
 	ptr = makeStudents(N);
+	cout << "Unsorted students: \n";
 	printStudents(ptr, N);
 	sortStudents(ptr, N);
+	cout << "\nSorted students: \n";
+	printStudents(ptr, N);
 
 }
 
-Students *makeStudents(int N)
-{
-	ifstream ifs;
+Students *makeStudents(int N) {
+	ifstream 	ifs;
     Students    *ptr= new Students [N];
 
     ifs.open("students.txt");
-    if ( ifs.fail())
-    {
+    if ( ifs.fail()) {
         cerr << "File open error\n";
         exit(0);
     }
 
-	for(int i=0;i<N; i++)
-    {
+	for(int i=0;i<N; i++) {
         ifs >> (ptr+i)->sid >> (ptr+i)->sname;
         for(int j=0; j<NUM_SCORES; j++)
 			ifs >> (ptr+i)->scores[j] ;
-		if ( ifs.fail() )
-		{
-			cerr << "File Read Error\n";
+		if ( ifs.fail() ) {
+			cerr << "File read error\n";
 			exit(0);
 		}
     }
 	return ptr;
+}
+
+void printStudents(Students *x, int N) {
+	cout <<"Student ID\tName\tScore1\tScore2\tScore3\tSum\n";
+	for (int i=0; i<N; i++) {
+		int sum = 0;
+		cout << x[i].sid << "\t" << x[i].sname << "\t";
+		for (int j=0; j<NUM_SCORES; j++) {
+        	cout << x[i].scores[j] << "\t";
+			sum += x[i].scores[j];
+		}
+		cout << sum << "\n";
+	}
+}
+
+void sortStudents(Students *x, int N) {
+    int sum1=0, sum2=0;
+    for( int i=0; i< N-1; i++ ) {
+	    for( int j=0; j<N-1; j++) {
+		    for(int k=0; k<3; k++) {
+                sum1 += x[j].scores[k];
+                sum2 += x[j+1].scores[k]; }    
+		    if ( sum1 > sum2)
+                swap(x[j], x[j+1]);
+			sum1 = 0;
+	        sum2 = 0;
+	    }
+    }
 }
