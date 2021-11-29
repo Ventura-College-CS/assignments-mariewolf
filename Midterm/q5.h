0bd81b344686f40aec518a80866a93c370e6fad4
mariewolf <77356063+mariewolf@users.noreply.github.com>
2021-11-29 15:26:20 -0800

    edited q-5

--- Midterm/q5.h
+++ Midterm/q5.h
@@ -1,6 +1,32 @@
 #ifndef Q5_H
 #define Q5_H
+#include <vector>
+using namespace std;
 
-//class definitions
+class Student {
+    private:
+        int SID;
+        string SName;
+        double scores[3];
+    public:
+        Student() : SID(0), SName() {}
+        Student(int i, string name, double *sc);
+        int getID();
+        string getName();
+        double *getScores();
+};
+
+template <class T>
+class Stack {
+    private:
+        vector<T> pool;
+    public:
+        Stack();
+        void push(const T &el);
+        T pop();
+        T &topEl();
+        bool isEmpty();
+        int size();
+};
 
 #endif
