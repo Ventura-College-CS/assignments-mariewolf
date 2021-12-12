f9b86e6cb9c3d46a0873de5e90446ecefe67f885
mariewolf <77356063+mariewolf@users.noreply.github.com>
2021-12-12 14:36:37 -0800

    edited q-1

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -1,21 +1,40 @@
 #include "final1.hpp"
 #include <fstream>
+#include <time.h>
 
-const int N = 10;
+const int N = 10; //number of courses
 
 void makeArray(Course []);
+int binarySearch(Course []);
+void printArray(Course []);
 
 int main() {
+    int target;
     Course arr[N];
     makeArray(arr);
+
+    srand(time(NULL));
+    int target = (arr[rand() % N]).getID(); //randomly choose target
+    cout << "\nTarget: " << target << endl;
 
     return 0;
 }
 
-void makeArray(Course arr) {
-    for (int i=0; i<N; i++) {
-        //read file
-    }
+void makeArray(Course arr[]) {
+    ifstream inFile("courses.txt");
+    if (inFile.fail()) {cerr << "File open error\n"; exit(0);} //file check
+    int id, cr;
+    string nm;
 
-    //sort array
+    for (int i=0; i<N; i++) { //read file and fill array
+        inFile >> id >> nm >> cr;
+        arr[i] = Course(id, nm, cr);
+        if (inFile.fail()) {cerr << "File read error\n"; exit(0);} //file check
+    }
+    inFile.close();
+
+    for (int i=0; i<N-1; i++) { //sort array
+        for (int j=i+1; j<N; j++)
+            if (arr[i].getID() > arr[j].getID()) swap(arr[i], arr[j]);
+    }
 }
