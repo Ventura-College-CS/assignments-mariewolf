d190e210a5560f2ef506dbf8d670b1591a2c0ce1
mariewolf <77356063+mariewolf@users.noreply.github.com>
2021-12-12 14:46:31 -0800

    edited q-1

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -5,23 +5,27 @@
 const int N = 10; //number of courses
 
 void makeArray(Course []);
-int binarySearch(Course []);
 void printArray(Course []);
+int binarySearch(Course [], int);
 
 int main() {
     int target;
     Course arr[N];
     makeArray(arr);
+    printArray(arr);
 
     srand(time(NULL));
     int target = (arr[rand() % N]).getID(); //randomly choose target
     cout << "\nTarget: " << target << endl;
 
+    int retIndex = binarySearch(arr, target);
+    //if found, return where
+    //if not, error
     return 0;
 }
 
 void makeArray(Course arr[]) {
-    ifstream inFile("courses.txt");
+    ifstream inFile("courses.txt"); //I created my own courses file; hope that's okay!
     if (inFile.fail()) {cerr << "File open error\n"; exit(0);} //file check
     int id, cr;
     string nm;
@@ -36,5 +40,21 @@ void makeArray(Course arr[]) {
     for (int i=0; i<N-1; i++) { //sort array
         for (int j=i+1; j<N; j++)
             if (arr[i].getID() > arr[j].getID()) swap(arr[i], arr[j]);
+    }
+}
+
+void printArray(Course arr[]) {
+    cout << "COURSE ID\tNAME\tCREDIT";
+    for (int i=0; i<N; i++)
+        cout << arr[i].getID() << "\t" << arr[i].getName() << "\t" << arr[i].getCredit() << endl; //using 'get' functions to print
+}
+
+int binarySearch(Course arr[], int target) {
+    int first, last, mid;
+    first = 0;
+    last = N-1;
+
+    while (first <= last) {
+
     }
 }
