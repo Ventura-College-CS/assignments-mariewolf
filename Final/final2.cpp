0e029423c070916e330cb7ead26d732aa000bfc8
mariewolf <77356063+mariewolf@users.noreply.github.com>
2021-12-12 15:01:38 -0800

    started q-2

---
+++ Final/final2.cpp
@@ -0,0 +1,49 @@
+#include "final1.hpp"
+#include <fstream>
+#include <time.h>
+const int N = 10; //number of courses
+
+void makeArray(Course []);
+void printArray(Course []);
+void qsort(Course [], int, int);
+void partition(Course [], int, int);
+
+//mostly copied from question 1
+
+int main() {
+    Course arr[N];
+    makeArray(arr);
+    printArray(arr);
+
+    srand(time(NULL));
+    int target = (arr[rand() % N]).getID(); //randomly choose target
+    cout << "\nPivot: " << target << endl;
+
+
+    return 0;
+}
+
+void makeArray(Course arr[]) {
+    ifstream inFile("courses.txt"); //I created my own courses file; hope that's okay!
+    if (inFile.fail()) {cerr << "File open error\n"; exit(0);} //file check
+    int id, cr;
+    string nm;
+
+    for (int i=0; i<N; i++) { //read file and fill array
+        inFile >> id >> nm >> cr;
+        arr[i] = Course(id, nm, cr);
+        if (inFile.fail()) {cerr << "File read error\n"; exit(0);} //file check
+    }
+    inFile.close();
+
+    for (int i=0; i<N-1; i++) { //sort array
+        for (int j=i+1; j<N; j++)
+            if (arr[i].getID() > arr[j].getID()) swap(arr[i], arr[j]);
+    }
+}
+
+void printArray(Course arr[]) {
+    cout << "Printing course array...\nID\tNAME\t\tCREDIT\n";
+    for (int i=0; i<N; i++)
+        cout << arr[i].getID() << "\t" << arr[i].getName() << "\t" << arr[i].getCredit() << endl; //using 'get' functions to print
+}
---
+++ Final/final2.hpp
@@ -0,0 +1,20 @@
+#ifndef FINAL1_HPP
+#define FINAL1_HPP
+#include <iostream>
+using namespace std;
+
+
+class Course { //copied from question 1
+    private:
+        int courseID;
+        string name;
+        int credit;
+    public:
+        Course() : courseID(0), name(" "), credit(0) {}
+        Course(int id, string nm, int cr) : courseID(id), name(nm), credit(cr) {}
+        int getID() {return courseID;}
+        string getName() {return name;}
+        int getCredit() {return credit;}
+};
+
+#endif
