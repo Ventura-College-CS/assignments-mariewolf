32df0eeb6d15b09ef7f111039306e367ad55ee8e
mariewolf <77356063+mariewolf@users.noreply.github.com>
2021-12-12 16:09:05 -0800

    edited q-3

--- Final/postfix.cpp
+++ Final/postfix.cpp
@@ -1,1 +1,62 @@
 #include "postfix.hpp"
+
+//mostly copied from previous assignment (5-2)
+
+using namespace std;
+
+int Postfix::isOperator(char op) {
+	switch (op)
+	{
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+int Postfix::calculate(int op1, int op2, char opr) {
+	int result;
+	switch (opr)
+	{
+	case '*':
+		result = op1 * op2;
+		break;
+	case '/':
+		result = op1 / op2;
+		break;
+	case '+':
+		result = op1 + op2;
+		break;
+	case '-':
+		result = op1 - op2;
+		break;
+	default:
+		cout << "Undefined operator error\n";
+		exit(0);
+	}
+	return result;
+}
+
+int Postfix::Evaluation() {
+    const int N = 20;
+	const int offset = 48;
+	Stack<int, N> stack;
+	char input;
+	int opr1, opr2, result;
+
+	for (int i = 0; i < expression.size(); i++) {
+		input = expression[i];
+		if (isdigit(input))
+			stack.push(int(input) - offset);
+		else if (isOperator(input)) {
+			opr1 = stack.pop();
+			opr2 = stack.pop();
+			result = calculate(opr1, opr2, input);
+			stack.push(result);
+		}
+	}
+    return stack.pop();
+}
--- Final/postfix.hpp
+++ Final/postfix.hpp
@@ -6,7 +6,12 @@ using namespace std;
 class Postfix {
     private:
         string expression;
+        int isOperator(char);
+        int calculate(int, int, char);
+    public:
+        Postfix() : expression(0) {}
+        Postfix(string ex) : expression(ex) {}
+        int evaluation();
 };
-
 
 #endif
